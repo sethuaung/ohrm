125d2fd1aab76708e779be8f1892b72c9d305810
James <sethuaung@outlook.com>
2026-08-13 19:14:50 +0630

    Delete Ecommerce directory

--- Ecommerce/resources/templates/frontend/products.php
+++
@@ -1,5 +0,0 @@
-<?php require_once('..\resources\functions.php'); ?>
-                        <div class="row">
-                <!--  was the product code but taken to function code-->
-                   <?php get_product();?>
-                </div><!-- row end here-->
--- Ecommerce/resources/templates/frontend/side_nav.php
+++
@@ -1,9 +0,0 @@
-<div class="col-md-3">
-<p class="lead">Muzuro Ecommerce</p>
-   <div class="list-group">
-       <?php
-           //  <--Category -->
-                get_categories();
-       ?>
-    </div>
- </div> 
